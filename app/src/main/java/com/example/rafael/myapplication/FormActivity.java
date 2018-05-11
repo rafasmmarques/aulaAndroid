8c523115bccac35c2105fc7901ce0c39b6d1d2a3
rafasmmarques <rafamarques93@hotmail.com>
2018-05-11 18:30:08 -0300

    Aula Alura

--- app/src/main/java/com/example/rafael/myapplication/FormActivity.java
+++ app/src/main/java/com/example/rafael/myapplication/FormActivity.java
@@ -1,5 +1,6 @@
 package com.example.rafael.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,11 +14,15 @@ public class FormActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
+        //botão de salvar o formulário
         Button botaoSalvar = (Button) findViewById(R.id.form_salvar);
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(FormActivity.this,"Formulário enviado com sucesso",Toast.LENGTH_SHORT).show();
+                Intent intentFormToMain = new Intent(FormActivity.this, MainActivity.class);
+                startActivity(intentFormToMain);
+                finish();
             }
         });
     }
--- app/src/main/java/com/example/rafael/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rafael/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.rafael.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
 
+        //botão que abre a view de Formulário
+        Button novoAluno = (Button) findViewById(R.id.novo_aluno);
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMainToForm = new Intent(MainActivity.this, FormActivity.class);
+                startActivity(intentMainToForm);
+            }
+        });
     }
 }
